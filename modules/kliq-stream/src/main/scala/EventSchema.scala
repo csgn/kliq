335875d46a3ce223f0bde47cb52cdd466ea38c5b
csgn <dev.csgn@gmail.com>
2025-02-08 16:44:47 +0300

    add default event schema

--- modules/kliq-stream/src/main/scala/EventSchema.scala
+++ modules/kliq-stream/src/main/scala/EventSchema.scala
@@ -6,7 +6,10 @@ object EventSchema {
   def apply() = {
     StructType(
       Array(
-        StructField("tv001", StringType, nullable = false)
+        StructField("id", StringType, nullable = false),
+        StructField("timestamp", StringType, nullable = false),
+        StructField("channel", StringType, nullable = false),
+        StructField("ipaddress", StringType, nullable = false),
       )
     )
   }
